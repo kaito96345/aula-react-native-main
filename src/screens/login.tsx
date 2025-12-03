faec36df734951b259b2fb2aa94fb3b24169ef06
Luis Otavio <luispaulosousa337@gmail.com>
2025-12-03 19:46:20 -0300

    feature add cadastre-se do usuario e fuçao de login

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -4,20 +4,27 @@ import { useState } from "react";
 import { Text, View } from "react-native";
 
 export function LoginScreen() {
-  const [Login, setlogin ]= useState('');
-  const [Senha, setSenha ]= useState('');
+  const [Login, setlogin] = useState('');
+  const [Senha, setSenha] = useState('');
+  function Logar() {
+    console.log("Login", Login, "senha", Senha)
+  }
   return (
     <View className="flex-1 items-center justify-center">
-      <View className='w-full p-5 gap-5 items-center justify-center'> 
+      <View className='w-full p-5 gap-5 items-center justify-center'>
         <Text className='text-3xl'>Login</Text>
 
-      <InputCustomizado placeholder={"Login"} onChangeText={(text)=> {setlogin(text)}}/>
-      <InputCustomizado placeholder={"Senha"}secureTextEntry={true}  onChangeText={(text)=> {setSenha(text)}}/>
-    
+        <InputCustomizado placeholder={"Login"} onChangeText={(text) => { setlogin(text) }} />
+        <InputCustomizado placeholder={"Senha"} secureTextEntry={true} onChangeText={(text) => { setSenha(text) }} />
 
-      <ButtonCustomizado title="Entrar" onPress={() => console.log("login", Login, "senha", Senha)}/>
-      <Text className='text-blue-400'> esqueceu a Senha?</Text>
-    </View>
+
+        <ButtonCustomizado title="Entrar" onPress={() => console.log("login", Login, "senha", Senha)} />
+        <View className='flex-row gap-5'>
+          <Text className='text-blue-400 text-lg' > Cadastre-se</Text>
+          <Text>|</Text>
+          <Text className='text-blue-400 text-lg'> esqueceu a Senha?</Text>
+        </View>
+      </View>
     </View>
   )
 }
