db1341fbf4e56043a70db2708e575c67f6cad371
Luis Otavio <luispaulosousa337@gmail.com>
2025-12-17 20:52:31 -0300

    feature add juçao de duas abas

--- App.tsx
+++ App.tsx
@@ -4,15 +4,19 @@ import { Lista } from '@/screens/lista';
 import { LoginScreen } from '@/screens/login';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Soma from '@/screens/soma';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <SafeAreaView className="flex-1">
-        
-
-     <Lista/>
-
+         <NavigationContainer>
+      <Stack.Navigator> 
+        <Stack.Screen name="Home" component={Lista} />
+      </Stack.Navigator>
+      </NavigationContainer>  
       </SafeAreaView>
 
       <StatusBar style="auto" />
