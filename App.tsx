e8536dada2ccfbf37f1c8588844969e35d3cd5fd
Luis Otavio <luispaulosousa337@gmail.com>
2025-12-19 20:19:20 -0300

    feature add lista com numeros,definir se e impar ou par

--- App.tsx
+++ App.tsx
@@ -6,22 +6,24 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Soma from '@/screens/soma';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
+import { RootStackParamList } from '@/interface/roolPages';
+import { ListaNumScreens } from '@/screens/numeros';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <>
       <SafeAreaView className="flex-1">
-      <NavigationContainer>
-      <Stack.Navigator> 
+      {/*<NavigationContainer>
+      <Stack.Navigator initialRouteName='login' screenOptions={{headerShown: false}}> 
         <Stack.Screen name="login" component={LoginScreen} />
         <Stack.Screen name="soma" component={Soma} />
         <Stack.Screen name="lista" component={Lista} />
-      </Stack.Navigator>
       </NavigationContainer>
+      </Stack.Navigator>*/}
+      <ListaNumScreens/>
       </SafeAreaView>
+
 
       <StatusBar style="auto" />
     </>
---
+++ src/screens/numeros.tsx
@@ -0,0 +1,58 @@
+import { ButtonCustomizado } from '@/components/botaoCustomizado';
+import { InputCustomizado } from '@/components/inputCustomizado';
+import { useState } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { useEffect } from 'react';
+
+export function ListaNumScreens() {
+  const [item, setItem] = useState('');
+  const [lista, setLista] = useState<number[]>([]);
+ const [resultado, setResultado] = useState<number | null>(0);
+  
+    useEffect(() => {
+      setResultado(0);
+    }, [item]);
+  
+
+  function AdicionarLista() {
+    if (item === '') return;
+    const numero = Number(item);
+    if (isNaN(numero)) return;
+
+    setLista([...lista, numero]);
+    setItem('');
+  }
+
+  function LimparLista() {
+    setLista([]);
+  }
+
+  return (
+    <View className="flex-1 items-center">
+      <View className="w-full items-center gap-4 p-4">
+        <Text className="text-3xl text-gray-700">Lista de Números</Text>
+
+        <InputCustomizado
+          placeholder="Digite um número"
+          keyboardType="numeric"
+          value={item}
+          onChangeText={setItem}
+        />
+        
+        <ButtonCustomizado title="Adicionar Número" onPress={AdicionarLista} />
+        <ButtonCustomizado title="Limpar Lista" onPress={LimparLista} />
+
+        <Text>Total de itens: {lista.length}</Text>
+      </View>
+
+      <ScrollView className="mt-4 w-full px-4">
+        {lista.map((numero, index) => (
+          <Text key={index} className="text-gray-600 text-lg">
+            {numero} {numero % 2 === 0 ? '(Par)' : '(Ímpar)'}
+        </Text>
+        ))}
+              <Text>Resultado: {resultado === 0 ? 'Par' : resultado === 1 ? 'Ímpar' : '...'}</Text>
+      </ScrollView>
+    </View>
+  );
+}
